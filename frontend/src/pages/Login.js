eb2f5d0c065ed7f3dfad4c993ecc2868ef5c3509
Iain <iain.hoolahan@gmail.com>
2022-12-02 13:21:35 +0000

    login gage and hook added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
   const [email, setEmail] = useState('') 
-  const [password, setPassword] = useState('') 
+  const [password, setPassword] = useState('')
+  const { login, error, isLoading } = useLogin()
 
   const handleSubmit = async (e) => {
     e.preventDefault() // prevent browser from refreshing page as default
 
-    console.log(email, password)
+    await login(email, password)
   }
   return (
     <form className="login" onSubmit={handleSubmit}>
@@ -24,8 +26,9 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button>Log in</button>
-    </form>
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
+    </form> // login button disabled while loading
   )
 }
 
